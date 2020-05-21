a0adb6cb2c7177f49ae9c1127c6199fb6bc064fc
dmjesus <diego.mesquita.jesus@gmail.com>
2020-05-21 16:15:45 -0300

    First commit
    Adding config file to set the virtual machine and adding docker compose
    file.

---
+++ config.sh
@@ -0,0 +1,6 @@
+########################################################
+# Docker toolbox default virtual-machine configuration.
+########################################################
+
+export MACHINE_STORAGE_PATH="/d/Softwares/virtual-boxes"
+docker-machine create -d virtualbox --virtualbox-memory 8192 default
